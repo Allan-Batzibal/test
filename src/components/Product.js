fdc8b4196040f8cd596acbf31ef82f09a6f220d9
isaac <desarrolloweb@disdelsa.com>
2021-02-16 09:48:10 -0600

    fdfdfdfd

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,8 @@
 import React from "react"
 
 const app = ({ allBooks }) => {
-  return "dddddfdfd"
+  console.log(allBooks)
+  return "hola"
 }
 
 export default app
--- src/templates/Productos.js
+++ src/templates/Productos.js
@@ -4,7 +4,7 @@ const SearchTemplate = props => {
   const { pageContext } = props
   const { bookData } = pageContext
   const { allBooks } = bookData
-  console.log(allBooks)
+
   return <ClientSearch allBooks={allBooks} />
 }
 export default SearchTemplate
